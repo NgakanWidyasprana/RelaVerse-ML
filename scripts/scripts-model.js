bc37c25034add4f1418a32bfeb385b2c38948c26
NgakanWidyasprana <ngakanputu39@gmail.com>
2023-06-08 01:53:30 +0800

    Update Test and Train Dataset

--- scripts/scripts-model.js
+++ scripts/scripts-model.js
@@ -1,15 +1,18 @@
-async function textClassifications() {
-    const inputText = "Kegiatan Pembersihan Lingkungan";
-    
-    const MODEL_URL = 'model/text-classification/model.json';
-    const model = await use.loadLayersModel(MODEL_URL);
-    
-    console.log(model.summary());
+require('@tensorflow/tfjs');
+const use = require('@tensorflow-models/universal-sentence-encoder');
 
-    const embeddings = await model.embed([inputText]);
-    const predictions = await model.predict(embeddings);
-    const positiveSentiment = predictions.arraySync()[0][0];
+function loadModel() {
+    const MODEL_URL = "/model/text-classification/model.json";
+    const model = loadLayersModel(MODEL_URL);
     
-    const sentimentResult = document.getElementById('classificationResult');
-    sentimentResult.textContent = `Sentiment: ${positiveSentiment}`;
+    // const embeddings = await model.embed([inputText]);
+    // const predictions = await model.predict(embeddings);
+    // const positiveSentiment = predictions.arraySync()[0][0];
+    
+    // const sentimentResult = document.getElementById('classificationResult');
+    // sentimentResult.textContent = `Sentiment: ${positiveSentiment}`;
+    return model;
 }
+
+model = loadModel();
+console.log(model.summary());
